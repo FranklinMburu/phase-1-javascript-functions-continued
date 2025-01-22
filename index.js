46e29a9189ab167e3769c3272fba8cdaefe410e8
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-01-22 23:22:08 +0300

    did the functions wrapAdjective was hard i din understand well

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+};
+
+
+
+function mondayWork (todo = "go to the office"){
+    return `This Monday, I will ${todo}.`;
+
+};
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
